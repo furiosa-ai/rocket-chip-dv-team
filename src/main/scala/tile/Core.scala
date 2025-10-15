79236e2313c104f2cf24bcaa03c7e07177ce211c
minho <minho@furiosa.ai>
2025-10-15 21:21:55 +0000

    enable commit log, print dasm and exception for riscv-dv

--- src/main/scala/tile/Core.scala
+++ src/main/scala/tile/Core.scala
@@ -147,7 +147,7 @@ trait HasCoreParameters extends HasTileParameters {
 
   // Print out log of committed instructions and their writeback values.
   // Requires post-processing due to out-of-order writebacks.
-  val enableCommitLog = false
+  val enableCommitLog = true
 
 }
 
